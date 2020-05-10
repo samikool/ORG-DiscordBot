016ca8383b04d5a2b9f26942c35c185a645c9fda
Sam <sam.morgan44@gmail.com>
2020-05-09 22:02:39 -0500

    adding files

---
+++ quizzer.js
@@ -0,0 +1,22 @@
+class Quizzer{
+    constructor(Discord, client){
+        this.Discord = Discord;
+        this.client = client;
+        this.quiztakers = [];
+    }
+    
+    addQuiztaker(){
+
+    }
+
+    removeQuiztaker(){
+
+    }
+
+    startQuiz(){
+
+    }
+
+}
+
+module.exports = Quizzer;
