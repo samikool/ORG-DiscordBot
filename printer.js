2b2a962b8fa8540840564ce3de17bfb7afa5d2f5
Sam Morgan <sam.morgan44@gmail.com>
2022-09-11 17:16:00 -0500

    Move away from ES6

--- printer.js
+++ printer.js
@@ -3,22 +3,29 @@ const yellow = "\u001b[33m"
 const white = "\u001b[37m"
 const green = "\u001b[32m"
 
-export function success(str){
+function success(str){
     print(str, "[SUCCESS]", green);
 }
 
-export function info(str){
+function info(str){
     print(str, "[INFO]", white)
 }
 
-export function warning(str){
+function warning(str){
     print(str, "[WARNING]", yellow)
 }
 
-export function error(str){
+function error(str){
     print(str, "[ERROR]", red)
 }
 
 function print(str, header, color){
     console.log(`${color}${header} ${white}${str}`)
+}
+
+module.exports = {
+    success,
+    info,
+    warning,
+    error
 }
