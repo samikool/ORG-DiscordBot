adcf5729004ac5581a8877d459b3b429c4ca8419
Sam Morgan <sam.morgan44@gmail.com>
2022-09-15 17:41:08 -0500

    Add github workflows for CI

--- ci/build.sh
+++ ci/build.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
-echo "Following is a build script:"
 env=${1}
 dotenv=${2}
-
-cd ..
 
 cp ${dotenv} .env
 
 build_args="--build-arg build_env=${env}"
-echo "${build_args}"
-docker build ${build_args} -t discordbot-env:${env} -f ci/Dockerfile.build-env .
+docker build -t discordbot-env:${env} -f ci/Dockerfile.build-env .
 docker build ${build_args} -t discordbot:${env} -f ci/Dockerfile .
-
-echo "Following should be a test script:"
-sleep 1
